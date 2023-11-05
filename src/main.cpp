23ca4194bde3cd5ae5c9811cf8ca858e8a5ec2e8
Jakub Binder <jakub-binder@email.cz>
2023-11-05 14:11:15 +0100

    Zmenen zpusob blikani led - uzita funkce millis(), pridana LED na pinu 23

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
+// ----------------------------------------------------------------
 // Testovano na:
 // Arduino IDE: ESP32_IoT: Board / ESP32 Wrower Module
 // VSCode: denky32
 // 
+//
+//
+// ----------------------------------------------------------------
 
 
 // ----------------------------------------------------------------
@@ -15,6 +19,7 @@
 // Pins
 //--------------------------------------------------------------
 #define LED5 5
+#define LED23 23
 
 
 
@@ -23,8 +28,13 @@
 //--------------------------------------------------------------
 struct Led 
 {
-  // state variables
+  // pin LEDky
   uint8_t pin;
+  // Promenna pro uchovani casu zmeny stavu
+  unsigned long previousMillis;
+  // Interval pro blikajici LED
+  const long interval;
+  // state variables
   bool on;
   // methods
   void update()
@@ -34,7 +44,26 @@ struct Led
 };
 
 // Deklarace ledek
-Led led_pin5 = { LED5, false };
+Led led_pin5 = { LED5, 0, 1000, false };
+Led led_pin23 = { LED23, 0, 600, false };
+
+
+// Kontrola, zda nema dojit ke zmene stavu LEDky
+void CheckLedBliking(int ledPin, long interval, unsigned long &previousMillis)
+{
+  // Zjisteni aktualniho casu
+  unsigned long currentMillis = millis();
+  // Kontrola zda uplynul uceny interval
+  if (currentMillis - previousMillis >= interval) {
+    // Ulozeni posledniho casu, kdy jsem blikali
+    previousMillis = currentMillis;
+
+    // Cteni stavu LED a nastaveni opacneho stavu
+    digitalWrite(ledPin, !digitalRead(ledPin));
+  }
+}
+
+
 
 //--------------------------------------------------------------
 // Setup
@@ -45,15 +74,19 @@ void setup()
 
   // ========================== Buttons and Leds ====================
   pinMode(led_pin5.pin, OUTPUT);
+  pinMode(led_pin23.pin, OUTPUT);
 }
 
 
+
+//--------------------------------------------------------------
+// Loop
+//-------------------------------------------------------------- 
 void loop() 
 {
-  led_pin5.on = !led_pin5.on;
-  led_pin5.update();
-
-  delay(500);
+  // LED Pin 5
+  CheckLedBliking(led_pin5.pin, led_pin5.interval, led_pin5.previousMillis);
+  CheckLedBliking(led_pin23.pin, led_pin23.interval, led_pin23.previousMillis);
 }
 
 
