0a2f0a5df622a65244a6bc19d96b22d5ae03e39e
Jakub Binder <jakub-binder@email.cz>
2023-11-05 20:02:37 +0100

    Pridany ctyri tlacitka, ctyri ledky a struktura pro vyhodnocovani tlacitek

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,8 @@
 // Pins
 //--------------------------------------------------------------
 #define LED5 5
+#define LED18 18
+#define LED19 19
 #define LED23 23
 
 // Buttons
@@ -49,6 +51,8 @@ struct Led
 
 // Deklarace ledek
 Led led_pin5 = { LED5, 0, 1000, false };
+Led led_pin18 = { LED18, 0, 1000, false };
+Led led_pin19 = { LED19, 0, 1000, false };
 Led led_pin23 = { LED23, 0, 600, false };
 
 
@@ -70,6 +74,65 @@ void CheckLedBliking(int ledPin, long interval, unsigned long &previousMillis)
 
 
 //--------------------------------------------------------------
+// Buttons
+//--------------------------------------------------------------
+// Button debouncing
+const uint8_t DEBOUNCE_DELAY = 10; // in milliseconds
+
+struct Button
+{
+  // state variables
+  uint8_t pin;                  // Definuje vstupní pin tlacitka
+  bool lastReading;             // Definuje posledni nactenou hodnotu signalu (HIGH nebo LOW)
+  uint32_t lastDebounceTime;    // Definuje poslední čas v milisekundách, kdy bylo provedeno poslední čtení
+  uint16_t state;               // 16bitové celé číslo, které definuje stav tlačítka.. (viz tutorial)
+
+  // metody určující logický stav tlačítak
+  bool pressed()                { return state == 1; }      // Stisknuto
+  bool released()               { return state == 0xffff; } // Uvolneno
+  bool held(uint16_t count = 0) { return state > 1 + count && state < 0xffff; } // Drzeno
+
+  // metoda pro cteni fyzickeo stavu tlacitka
+  void read()
+  {
+    // přečte napětí na pinu připojeném k tlačítku
+    bool reading = digitalRead(pin);
+
+    // Pokud se logická úroveň od posledního čtení změnila,
+    // resetujeme časovač, který odpočítává potřebný čas
+    // za nímž můžeme uvažovat o efektu skákání prošel.
+    if (reading != lastReading) 
+    {
+      lastDebounceTime = millis();
+    }
+    // od chvíle, kdy jsme mimo fázi skákání
+    // lze zjistit aktuální stav tlačítka
+    if (millis() - lastDebounceTime > DEBOUNCE_DELAY) 
+    {
+      // don't forget that the read pin is pulled-up
+      // ToDo Kontrola - zda to nemam naopak
+      bool pressed = reading == HIGH;
+      if (pressed) 
+      {
+        if (state  < 0xfffe) state++;
+        else if (state == 0xfffe) 
+          state = 2;
+      } 
+      else if (state) 
+      {
+        state = state == 0xffff ? 0 : 0xffff;
+      }
+    }
+    // finally, each new reading is saved
+    lastReading = reading;
+  }
+};
+Button button1 = { BUTT1, HIGH, 0, 0 };
+Button button2 = { BUTT2, HIGH, 0, 0 };
+Button button3 = { BUTT3, HIGH, 0, 0 };
+Button button4 = { BUTT4, HIGH, 0, 0 };
+
+//--------------------------------------------------------------
 // Setup
 //-------------------------------------------------------------- 
 void setup() 
@@ -78,7 +141,14 @@ void setup()
 
   // ========================== Buttons and Leds ====================
   pinMode(led_pin5.pin, OUTPUT);
+  pinMode(led_pin18.pin, OUTPUT);
+  pinMode(led_pin19.pin, OUTPUT);
   pinMode(led_pin23.pin, OUTPUT);
+
+  pinMode(button1.pin, INPUT);
+  pinMode(button2.pin, INPUT);
+  pinMode(button3.pin, INPUT);
+  pinMode(button4.pin, INPUT);
 }
 
 
@@ -88,9 +158,35 @@ void setup()
 //-------------------------------------------------------------- 
 void loop() 
 {
-  // LED Pin 5
-  CheckLedBliking(led_pin5.pin, led_pin5.interval, led_pin5.previousMillis);
-  CheckLedBliking(led_pin23.pin, led_pin23.interval, led_pin23.previousMillis);
+  // Blikáni ledek
+  //CheckLedBliking(led_pin5.pin, led_pin5.interval, led_pin5.previousMillis);
+  //CheckLedBliking(led_pin23.pin, led_pin23.interval, led_pin23.previousMillis);
+
+  button1.read();
+  button2.read();
+  button3.read();
+  button4.read();
+
+  if (button1.pressed())
+  {
+    led_pin5.on = !led_pin5.on;
+    led_pin5.update();
+  }
+  if (button2.pressed())
+  {
+    led_pin18.on = !led_pin18.on;
+    led_pin18.update();
+  }
+  if (button3.pressed())
+  {
+    led_pin19.on = !led_pin19.on;
+    led_pin19.update();
+  }
+  if (button4.pressed())
+  {
+    led_pin23.on = !led_pin23.on;
+    led_pin23.update();
+  }
 }
 
 
